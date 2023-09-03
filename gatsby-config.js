dbd50703a35e1c964fc2b191701ed6080e6db62a
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 00:21:34 +0600

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,8 +54,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: mcsbhugd3yek,
+        accessToken: HGIY5Z07k8wfSd7r0yYGKMhjDmDKnUaIfo8d3QeuOu0,
       },
     },
     {
